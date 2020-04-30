719e4b260e19b83ec876006ea0307de101837696
Darren Ross <darren.ross@bellevuecollege.edu>
2020-04-30 16:21:27 -0700

    Added TODO item in line.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,7 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    //TODO: Send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
